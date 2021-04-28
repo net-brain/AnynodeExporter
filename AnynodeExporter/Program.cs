0591d40f94b34103ea2117607b45431fdbc3d669
Lorenz Buchberger <l.buchberger@net-brain.it>
2021-04-28 18:19:24 +0200

    added docker-ompose

--- AnynodeExporter/Program.cs
+++ AnynodeExporter/Program.cs
@@ -18,6 +18,10 @@ namespace AnynodeExporter
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables("ae_");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
